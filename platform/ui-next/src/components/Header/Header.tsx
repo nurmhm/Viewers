58926425628a3be7d0e8d3535f078a5848026f20
Nur Mohammad <nurmd7228@gmail.com>
2025-08-07 19:21:41 +0600

    fix: header fix

--- platform/ui-next/src/components/Header/Header.tsx
+++ platform/ui-next/src/components/Header/Header.tsx
@@ -61,7 +61,7 @@ function Header({
         {...props}
       >
         <div className="relative h-[48px] items-center">
-          <div className="absolute left-0 top-1/2 flex -translate-y-1/2 items-center">
+          {/* <div className="absolute left-0 top-1/2 flex -translate-y-1/2 items-center">
             <div
               className={classNames(
                 'mr-3 inline-flex items-center',
@@ -70,14 +70,15 @@ function Header({
               onClick={onClickReturn}
               data-cy="return-to-work-list"
             >
-              {isReturnEnabled && <Icons.ArrowLeft className="text-primary ml-1 h-7 w-7" />}
+              <p>hello this is my header </p>
+             {isReturnEnabled && <Icons.ArrowLeft className="text-primary ml-1 h-7 w-7" />}
               <div className="ml-1">
                 {WhiteLabeling?.createLogoComponentFn?.(React, props) || <Icons.OHIFLogo />}
               </div>
             </div>
-          </div>
-          <div className="absolute top-1/2 left-[250px] h-8 -translate-y-1/2">{Secondary}</div>
-          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+          </div> */}
+          {/* <div className="absolute top-1/2 left-[250px] h-8 -translate-y-1/2">{Secondary}</div> */}
+          <div className="absolute left-0 top-1/2 flex -translate-y-1/2 items-center">
             <div className="flex items-center justify-center space-x-2">{children}</div>
           </div>
           <div className="absolute right-0 top-1/2 flex -translate-y-1/2 select-none items-center">
@@ -85,27 +86,29 @@ function Header({
             <div className="border-primary-dark mx-1.5 h-[25px] border-r"></div>
             {PatientInfo}
             <div className="border-primary-dark mx-1.5 h-[25px] border-r"></div>
-            <div className="flex-shrink-0">
+            <div className="flex-shrink-0 bg-red-500">
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                   <Button
                     variant="ghost"
                     size="icon"
-                    className="text-primary hover:bg-primary-dark mt-2 h-full w-full"
+                    className="text-primary hover:bg-red-500 mt-2 h-full w-full"
                   >
                     <Icons.GearSettings />
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
                   {menuOptions.map((option, index) => {
+
                     const IconComponent = option.icon
                       ? Icons[option.icon as keyof typeof Icons]
                       : null;
                     return (
+
                       <DropdownMenuItem
                         key={index}
                         onSelect={option.onClick}
-                        className="flex items-center gap-2 py-2"
+                        className="flex items-center gap-2 py-2 bg-rd"
                       >
                         {IconComponent && (
                           <span className="flex h-4 w-4 items-center justify-center">
